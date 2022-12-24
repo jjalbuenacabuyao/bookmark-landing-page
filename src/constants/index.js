ed95b0e4094496ce17506f48be92fa708c40530c
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-12-24 16:24:00 +0800

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+export const header = {
+  buttonText: "LOGIN"
+}
+
+export const links = ["Features", "Pricing", "Contact"]
+
+export const hero = {
+  title: "A Simple Bookmark Manager",
+  content: "A clean and simple interface to organize your favorite websites. Open a new browser tab and see your sites load instantly. Try it for free.",
+  btn: [
+    "Get it on Chrome", 
+    "Get it on FireFox"
+  ]
+}
+
+export const features = {
+  title: "Features",
+  content: "Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.",
+  tabTitles: [
+    "Simple Bookmarking",
+    "Speedy Searching",
+    "Easy Sharing"
+  ],
+  tabHeadings: [
+    "Bookmark in one Click",
+    "Intelligent search",
+    "Share your bookmarks"
+  ],
+  tabContents: [
+    "Organize your bookmarks however you like. Our simple drag-and drop inerface gives you complete control over how you manage your favorite sites.",
+    "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+    "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button."
+  ],
+  btn: "More Info"
+}
+
+export const download = {
+  title: "Download the extension",
+  content: "We've got more browsers in the pipeline. Please do let us know if you've got a favorite you'd like us to prioritize.",
+  btnTextContent: "Add & Install Extension",
+  browserTitle: [
+    "Add to Chrome",
+    "Add to Firefox",
+    "Add to Opera"
+  ],
+  browserContent: [
+    "Minimum version 62",
+    "Minimum version 55",
+    "Minimum version 46"
+  ]
+}
+
+export const faq = {
+  title: "Frequently Asked Questions",
+  content: "Here are some of our FAQs. If you have any other questions you'd like answered please feel free to email us.",
+  questions: [
+    "What is Bookmark?",
+    "How can I request a new browser?",
+    "Is there a mobile app?",
+    "What about other Chromium browsers?"
+  ],
+  dummyText: "lorem",
+  btn: "More Info"
+}
+
+export const cta = {
+  title: "Stay up-to-date with what we're doing",
+  joined: "35,000+ Already Joined",
+  btn: "Contact Us",
+  placeholder: "Enter your email address"
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "pr-soft-blue": "hsl(231, 69%, 60%)",
+        "pr-soft-red": "hsl(0, 94%, 66%)",
+        "nt-grayish-blue": "hsl(229, 8%, 60%)",
+        "nt-dark-blue": "hsl(229, 31%, 21%)"
+      },
+      fontFamily: {
+        "rubik": ["Rubik", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
